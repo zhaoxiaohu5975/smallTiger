baf4274548be549c5640aaf7df57dcd842b1cfa7
赵晓 <1005425637@qq.com>
2021-05-14 13:19:16 +0800

    更新代码

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -8,10 +8,7 @@ Page({
   onLoad() {
     this.setData({
       logs: (wx.getStorageSync('logs') || []).map(log => {
-        return {
-          date: util.formatTime(new Date(log)),
-          timeStamp: log
-        }
+        return util.formatTime(new Date(log))
       })
     })
   }
